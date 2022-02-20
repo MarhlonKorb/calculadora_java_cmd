f76d11ede75cb22b6556fbaf7d310576454e63e6
Marhlon Korb <marhlonkorb@hotmail.com>
2022-02-20 00:12:14 -0300

    Interface Predicate para verificação de valores que retorna um boolean. Pode ser utilizado para verificar valores em uma lista de itens por exemplo.

---
+++ lambdas/src/Predicado.java
@@ -0,0 +1,11 @@
+import java.util.function.Predicate;
+
+public class Predicado {
+    public static void main(String[] args) {
+        Predicate <Produto> isCaro =
+                produto ->  (produto.preco * (1 - produto.desconto)) >= 750;
+
+        Produto produto = new Produto("Notebook",3899, 0.15);
+        System.out.println(isCaro.test(produto));
+    }
+}
---
+++ lambdas/src/Produto.java
@@ -0,0 +1,11 @@
+class Produto {
+    String nome;
+    double preco;
+    double desconto;
+
+    public Produto(String nome, double preco, double desconto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+}
