b6fefeda2db7860096d548e8f7e87dd11933dfba
Marhlon Korb <marhlonkorb@hotmail.com>
2022-05-26 14:37:33 -0300

    Aplicação do conceito da classe StringBuilder e StringBuffer. A diferença entre essas classes para uma classe String é que a classe String cria objetos a serem concatenados enquanto que as classes Builder e Buffer contém o método append(), que apenas concatena as strings sem criar objetos, melhorando o tempo e capacidade de processamento da aplicação.

--- StringBuilderAplicacao/src/BuilderEBuffer.java
+++ StringBuilderAplicacao/src/BuilderEBuffer.java
@@ -6,6 +6,17 @@ public class BuilderEBuffer {
         StringBuilder frase2 = new StringBuilder(frase);
         frase2.append(" e tenho").append(" 27 anos");
 
+        //insere caracter em uma frase ou palavra
+        frase2.insert(37, ".");
+
+        //exclui campos entre os numeros selecionados / insert insere campos na string
+        frase2.delete(21,37).insert(21,".Adeus");
+
+        //reverte a frase ou palavra
+        frase2.reverse();
         System.out.println(frase2);
+
+        System.out.println(frase2.substring(25,28));
+
     }
 }
